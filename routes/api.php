aac8b23b4a3a7d725f3d22ceb05a2bb39eda9745
Andrew-Mtz <andrew.nahuelMH@gmail.com>
2021-08-10 00:14:09 -0300

    add controller and model

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,34 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Account;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/* Route::post('accounts', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return Account::all();
+}); */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('balance/{id}', function($id) {
+    return Account::find($id);
+});
+
+Route::post('deposito', function(Request $request, $id) {
+    $account = Account::findOrFail($id);
+    $account->create($request->all());
+
+    return $account;
+});
+
+Route::post('retiro', function(Request $request, $id) {
+    $account = Account::findOrFail($id);
+    $account->update($request->all());
+
+    return $account;
+});
+
+Route::delete('accounts/{id}', function($id) {
+    Account::find($id)->delete();
+
+    return 204;
 });
