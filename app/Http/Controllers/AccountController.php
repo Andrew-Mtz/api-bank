8263b3aa711b9914e4449ace0124107755a9dad0
Andrew-Mtz <andrew.nahuelMH@gmail.com>
2021-08-30 02:51:51 -0300

    endpoints creados

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -19,36 +19,89 @@ class AccountController extends Controller
 
     public function store(Request $request)
     {
-        $request->validate([
-            'email' => 'required|string|email|max:255|unique:accounts'
-        ]);
-        $account = Account::create($request->all());
-        return response()->json($account, 201);
+        try {
+            $request->validate([
+                'email' => 'required|string|email|max:255|unique:accounts'
+            ]);
+            $account = new Account();
+            $account->email = $request->input('email');
+            $account->balance = 0;
+            $account->save();
+            return "cuenta creada correctamente";
+        } catch (\Illuminate\Database\QueryException $e) {
+            return "Error";
+        }
     }
 
-    public function update(Request $request, $id)
+    public function deposito(Request $request)
     {
-/*         switch ($evento) {
-            case 'deposito':
-                $account = Account::find($id);
-                echo $account;
-                break;
-            case 'retiro':
-                # code...
-                break;
-            case 'transferencia':
-                # code...
-                break;
-                        
-            default:
-                # code...
-                break;
-        } */
-        $account = Account::find($id);
-        $account->update($request->all());
-        echo $request;
-/*         $account->update($request->all());
-        return response()->json($account, 200); */
+        $id = $request->input('id');
+        $account = Account::where('id', $id)->first();
+        try {
+            if ($account) {
+                $nuevoBalance = $account->balance + $request->input('monto');
+                $account->balance = $nuevoBalance;
+                $account->save();
+                return response()->json("balance actualizado correctamente", 200);
+            } else {
+                return response()->json("La cuenta con el id proporcionado no existe", 404);
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            return "Error";
+        } 
+    }
+
+    public function retiro(Request $request)
+    {
+        $id = $request->input('id');
+        $account = Account::where('id', $id)->first();
+        try {
+            if ($account) {
+                $monto = $request->input('monto');
+                if ($account->balance >= $monto && $monto >= 1000) {
+                /* aaaaaaaaaaaaa */
+                } else if ($account->balance >= $monto) {
+                    $nuevoBalance = $account->balance - $monto;
+                    $account->balance = $nuevoBalance;
+                    $account->save();
+                    return response()->json("retiro realizado correctamente", 200);
+                } else {
+                    return response()->json("El monto a retirar es mayor al balance de la cuenta", 403);
+                }
+            } else {
+                return response()->json("La cuenta con el id proporcionado no existe", 404);
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            return "Error";
+        } 
+    }
+
+    public function transferencia(Request $request)
+    {
+        $idOrigen = $request->input('idOrigen');
+        $originAccount = Account::where('id', $idOrigen)->first();
+        $idDestino = $request->input('idDestino');
+        $destinationAccount = Account::where('id', $idDestino)->first();
+        try {
+            if ($originAccount && $destinationAccount) {
+                $monto = $request->input('monto');
+                if ($originAccount->balance >= $monto && $monto >= 1000) {
+                    echo "en progreso";
+                } else if ($originAccount->balance >= $monto) {
+                    $destinationAccount->balance = $destinationAccount->balance + $monto;
+                    $destinationAccount->save();
+                    $originAccount->balance = $originAccount->balance - $monto;
+                    $originAccount->save();
+                    return response()->json("transferencia realizada correctamente", 200);
+                }else {
+                    return response()->json("El monto a transferir es mayor al balance de la cuenta", 403);
+                }
+            } else {
+                return response()->json("Los id proporcionados de las cuentas son erroneos", 404);
+            }
+        } catch (\Illuminate\Database\QueryException $e) {
+            return "Error";
+        } 
     }
 
     public function destroy($id)
--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,14 @@ use App\Http\Controllers\AccountController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/* Route::get('/accounts' , [AccountController::class, 'index']);
+ Route::get('/account' , [AccountController::class, 'index']);
 
-Route::post('/accounts' , [AccountController::class, 'store']);
+Route::post('/account' , [AccountController::class, 'store']);
 
-Route::post('/accounts' , [AccountController::class, 'update']); */
+Route::put('/account/deposito' , [AccountController::class, 'deposito']); 
 
-Route::resource('account', AccountController::class);
+Route::put('/account/retiro' , [AccountController::class, 'retiro']); 
+
+Route::put('/account/transferencia' , [AccountController::class, 'transferencia']); 
+
+//Route::resource('account', AccountController::class);
